9a86e69d8faacc8d85b9fee4b13ede6d020e8e8e
Didiel Figueroa <didiel.f@gmail.com>
2021-06-22 08:40:07 -0400

    12 commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Redirect,
 } from "react-router-dom";
 import './App.css';
 
 import { MainScreen } from './components/MainScreen';
 import Cart from './components/Cart/Cart';
 
+const settingsGH_pages = window.location.pathname;
+
+//////////////////  PARA PROBAR EN LOCAL SOLO SE DEBE REEMPLAZAR LA VARIABLE settingsGH_pages POR "/" o simplemente eliminar la propiedad basename
 
 function App() {
   return (
-    <Router basename ="/">
+    <Router basename={settingsGH_pages}>
       <div className="app">
         <Switch>
           <Route exact path="/" component={MainScreen} />
